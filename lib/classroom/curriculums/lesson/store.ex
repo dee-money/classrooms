2ad7815a5afc92c9de2aa02085c1ff304d26aa38
Justin C. Keys <61174616+sirLionFish@users.noreply.github.com>
2021-06-18 14:09:13 +0700

    clean up

--- lib/classroom/curriculums/lesson/store.ex
+++ lib/classroom/curriculums/lesson/store.ex
@@ -1,24 +1,23 @@
-defmodule Classroom.Curriculums.Lesson.Store do
+defmodule Classroom.Venues.Building.Store do
   use Agent
-
-  alias Classroom.Curriculums.Lesson
+  alias Classroom.Venues.Building
 
   def start_link(_) do
     Agent.start_link(fn -> [] end, name: __MODULE__)
   end
 
-  def add(%Lesson{} = lesson) do
-    Agent.update(__MODULE__, fn state -> [lesson | state] end)
+  def add(%Building{} = building) do
+    Agent.update(__MODULE__, fn state -> [building | state] end)
   end
 
   def all do
     Agent.get(__MODULE__, fn state -> state end)
   end
 
-  def get(lesson_id) do
-    Agent.get(__MODULE__, fn state -> 
-      Enum.find(state, fn lesson -> 
-        lesson.id == lesson_id 
+  def get(building_id) do
+    Agent.get(__MODULE__, fn state ->
+      Enum.find(state, fn building ->
+        building.id == building_id
       end)
     end)
   end
