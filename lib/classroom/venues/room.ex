5f189bd533d0d86cf1fc2867886a0917f9088adc
Justin C. Keys <61174616+sirLionFish@users.noreply.github.com>
2021-06-18 14:08:14 +0700

    create room

--- lib/classroom/venues/room.ex
+++ lib/classroom/venues/room.ex
@@ -1,3 +1,20 @@
 defmodule Classroom.Venues.Room do
-  defstruct [:id, :number, :building_id]
+  @derive Jason.Encoder
+  defstruct [:id, :room, :lesson, :pupil, :instructor,]
+
+  def new(%{room: room, pupil: pupil}) do
+    %__MODULE__{id: UUID.uuid4(), room: room, pupil: pupil}
+  end
+
+  def add_pupil(room, pupil) do
+    %{room | room: room.pupil ++ pupil}
+  end
+
+  def add_instructor(room, instructor) do
+    %{room | instructor: instructor}
+  end
+
+  def add_lesson(room, lesson) do
+    %{room | lesson: lesson}
+  end
 end
