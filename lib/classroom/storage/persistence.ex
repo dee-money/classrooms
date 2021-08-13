05c315df602a433f48c0002102e3e252f0d350a2
Zack Siri <zacksiri@gmail.com>
2021-08-13 17:23:19 +0700

    add association handling and loading data into memory on boot

--- lib/classroom/storage/persistence.ex
+++ lib/classroom/storage/persistence.ex
@@ -2,37 +2,54 @@ defmodule Classroom.Storage.Persistence do
   def load(module) do
     path(module)
     |> File.read()
-    |> case do 
-      {:ok, content} -> 
+    |> case do
+      {:ok, content} ->
         content
         |> Jason.decode!()
         |> Enum.map(fn param ->
           param
-          |> Enum.map(fn {key, value} -> 
+          |> Enum.map(fn {key, value} ->
+            value = maybe_parse_association(value)
+
             {String.to_existing_atom(key), value}
           end)
           |> Enum.into(%{})
         end)
-        |> Enum.map(fn param -> 
+        |> Enum.map(fn param ->
           struct(module, param)
         end)
-        
-      {:error, _} -> []
+
+      {:error, _} ->
+        []
     end
   end
-  
+
   def persist(module, content) do
     path(module)
     |> File.write!(Jason.encode!(content))
   end
-  
+
   defp path(module) do
-    module_name = 
+    module_name =
       Macro.underscore(module)
       |> String.split("/")
       |> List.last()
-    
+
     Application.app_dir(:classroom)
     |> Path.join("priv/stores/#{module_name}.json")
   end
+
+  defp maybe_parse_association(%{
+    "module" => module, 
+    "resource_id" => resource_id
+  }) do
+    module = String.to_atom(module)
+
+    %Classroom.Storage.Association{
+      module: module,
+      resource_id: resource_id
+    }
+  end
+
+  defp maybe_parse_association(value), do: value
 end
